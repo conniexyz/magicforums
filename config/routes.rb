f51943920d448f9606d327d6abe138b7ef30b16f
conniexyz <connie.d33pmatrix.com>
2016-08-17 00:12:19 +0800

    W1D3 The Rails Console & Database Mgmt

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-root to: 'landing#index'
-get :about, to: 'static_pages#about'
+  #Added by CW:
+  root to: 'landing#index'
+  get :about, to: 'static_pages#about'
+
+  resources :topics
 
 end
